599d81936c77dd27251ffbbd5cf498c85fd39f49
AUGMENTARO <109080715+AUGMENTARO@users.noreply.github.com>
2022-09-30 19:18:25 +1000

    1 УРОК
    Сделайте индексированный массив, где каждый элемент будет ассоциативным массивом, который будет описывать каждого человека из нашей группы)

---
+++ Index.php
@@ -0,0 +1,37 @@
+<?php 
+
+$users = [
+    [
+        'name' => "Dmitriy",
+        'surname' => "Labzov",
+        'age' => 10,
+        'is_developer' => true,
+        'languages' => [html,css,JS,php],
+    ],
+    [
+        'name' => "Dmitriy",
+        'surname' => "Gorbunov",
+        'age' => 10,
+        'is_developer' => true,
+        'languages' => [html,css,JS,php],
+    ],
+    [
+        'name' => "Ilya",
+        'surname' => "Vorobey",
+        'age' => 10,
+        'is_developer' => true,
+        'languages' => [html,css,JS,php],
+    ],
+    [
+        'name' => "Denis",
+        'surname' => "Popov",
+        'age' => 10,
+        'is_developer' => true,
+        'languages' => [html,css,JS,php],
+    ],
+    
+];
+
+print('<pre>');
+print_r($users);
+print('</pre>');
