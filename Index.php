6053eb35b926af92b2e3e761b7fef83fb022f7a4
AUGMENTARO <109080715+AUGMENTARO@users.noreply.github.com>
2022-10-02 10:39:10 +1000

    Add files via upload
    1. Написать функцию, которая будет возвращать одного user’а (из первого задания) в виде ассоциативного массива
    2. Написать функцию, которая будет возвращать массив user’ов, используя функцию из первого пункта + цикл For() или while()
    3. Наполнить массив $users (из первого задания) программным способом, используя функцию из 2 пункта(WIP)

--- Index.php
+++ Index.php
@@ -28,6 +28,36 @@ $users = [
     
 ];
 
+// print('<pre>');
+// print_r($users);
+// print('</pre>');
+
+// 1. Написать функцию, которая будет возвращать одного user’а (из первого задания) в виде ассоциативного массива.
+function getUser()
+
+{
+    return     [
+        'name' => "Dmitriy",
+        'surname' => "Labzov",
+        'age' => 16,
+        'is_developer' => true,
+    ];
+}
+
 print('<pre>');
-print_r($users);
+print_r(getUser());
+print('</pre>');
+
+// 2. Написать функцию, которая будет возвращать массив user’ов, используя функцию из первого пункта + цикл for() или while()
+function getUsers()
+{
+    $usersList = [];
+    while (count($usersList) <= 4) {
+        array_push($usersList, getUser());
+    }
+    return $usersList;
+}
+
+print('<pre>');
+print_r(getUsers());
 print('</pre>');
